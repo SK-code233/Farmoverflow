dc2026be073461a4cdec2eda48a190d8d7abacb3
Henry Quartey <henryfoli36@gmail.com>
2022-10-08 14:31:39 +0000

    firebase

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,21 @@
+const firebaseConfigss = {
+  apiKey: "AIzaSyALe-ngAbEo4qZlG4tVHs0RACBsVFRi5fo",
+  authDomain: "farm-2427d.firebaseapp.com",
+  projectId: "farm-2427d",
+  storageBucket: "farm-2427d.appspot.com",
+  messagingSenderId: "919603634379",
+  appId: "1:919603634379:web:3ac342398bc2ab533a7b4e",
+  measurementId: "G-J86WC5R3L6"
+};
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCalXDob3ExuRrDaIVaQM4LWExHWW6agZQ",
+  authDomain: "farmstack-556df.firebaseapp.com",
+  projectId: "farmstack-556df",
+  storageBucket: "farmstack-556df.appspot.com",
+  messagingSenderId: "579589603082",
+  appId: "1:579589603082:web:b0a83e9ad6abce6d6144f3",
+  measurementId: "G-462S1G4X7Z"
+};
+
+export default firebaseConfig
